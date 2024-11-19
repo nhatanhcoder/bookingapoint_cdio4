0423509caf28f7141b66e9971714e1792e744236
Nhat Anh <nhatanhkof@gmail.com>
2024-11-19 16:00:05 +0700

    delete files

--- src/pages/Apointment.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Apointment = () => {
-  return (
-    <div>Apointment</div>
-  )
-}
-
-export default Apointment
--- src/pages/MyApointments.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MyApointments = () => {
-  return <div>MyApointments</div>;
-};
-
-export default MyApointments;
